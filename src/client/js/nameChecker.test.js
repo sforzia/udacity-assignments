0da4fdfcd4c3351f69cd354edf84bbdf48141224
Syed Ziaul Ameen <syed.ameen@magicedtech.com>
2021-12-15 23:57:36 +0530

    emptied master branch

--- src/client/js/nameChecker.test.js
+++
@@ -1,8 +0,0 @@
-import { checkForName } from "./nameChecker";
-
-describe("nameChecker tests", () => {
-  test("testing checkForName", () => {
-    const expectedResult = "Welcome, Captain!";
-    expect(checkForName("Archer")).toBe(expectedResult);
-  });
-});
